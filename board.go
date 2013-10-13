7f374cdd63be437eae65d4d77c0b239065bf38d3
Dave Foley <davidmfoley@gmail.com>
2013-10-12 17:51:15 -0700

    gofmt all the things

--- board.go
+++ board.go
@@ -1,20 +1,20 @@
 package life
 
 type Board struct {
-  cells Cells
+	cells Cells
 }
 
 func (board Board) Evolve() Board {
-  var nursery = board.buildNursery()
-  return Board{nursery.NextGeneration()}
+	var nursery = board.buildNursery()
+	return Board{nursery.NextGeneration()}
 }
 
 func (board Board) buildNursery() Nursery {
-  var nursery = Nursery{}
-  nursery.MarkCells(board.Cells())
-  return nursery
+	var nursery = Nursery{}
+	nursery.MarkCells(board.Cells())
+	return nursery
 }
 
 func (board Board) Cells() Cells {
-  return board.cells
+	return board.cells
 }
--- life_test.go
+++ life_test.go
@@ -5,75 +5,74 @@ import "fmt"
 import "sort"
 
 func TestNeighbors(t *testing.T) {
-  var neighbors = Cell{0, 0}.Neighbors()
-  var expected = []Cell{
-    Cell{-1, -1},
-    Cell{-1, 0},
-    Cell{-1, 1},
-    Cell{0, -1},
-    Cell{0, 1},
-    Cell{1, -1},
-    Cell{1, 0},
-    Cell{1, 1},
-  }
-  AssertCellsEqual(expected, neighbors, t)
+	var neighbors = Cell{0, 0}.Neighbors()
+	var expected = []Cell{
+		Cell{-1, -1},
+		Cell{-1, 0},
+		Cell{-1, 1},
+		Cell{0, -1},
+		Cell{0, 1},
+		Cell{1, -1},
+		Cell{1, 0},
+		Cell{1, 1},
+	}
+	AssertCellsEqual(expected, neighbors, t)
 }
 
 func TestOscillator(t *testing.T) {
-  var starting = Cells{
-    Cell{0, 0},
-    Cell{0, 1},
-    Cell{0, 2},
-  }
-  var expected = Cells{
-    Cell{1, 1},
-    Cell{-1, 1},
-    Cell{0, 1},
-  }
+	var starting = Cells{
+		Cell{0, 0},
+		Cell{0, 1},
+		Cell{0, 2},
+	}
+	var expected = Cells{
+		Cell{1, 1},
+		Cell{-1, 1},
+		Cell{0, 1},
+	}
 
-  CheckEvolution(t, starting, expected)
-  CheckEvolution(t, expected, starting)
+	CheckEvolution(t, starting, expected)
+	CheckEvolution(t, expected, starting)
 }
 
 func TestBox(t *testing.T) {
-  var box = Cells{
-    Cell{23, 26},
-    Cell{24, 26},
-    Cell{23, 27},
-    Cell{24, 27},
-  }
+	var box = Cells{
+		Cell{23, 26},
+		Cell{24, 26},
+		Cell{23, 27},
+		Cell{24, 27},
+	}
 
-  CheckEvolution(t, box, box)
+	CheckEvolution(t, box, box)
 }
 
 func TestDiamond(t *testing.T) {
-  var box = Cells{
-    Cell{23, 26},
-    Cell{24, 25},
-    Cell{25, 26},
-    Cell{24, 27},
-  }
+	var box = Cells{
+		Cell{23, 26},
+		Cell{24, 25},
+		Cell{25, 26},
+		Cell{24, 27},
+	}
 
-  CheckEvolution(t, box, box)
+	CheckEvolution(t, box, box)
 }
 
 func CheckEvolution(t *testing.T, starting Cells, expected Cells) {
-  var evolved = Board{starting}.Evolve()
-  AssertCellsEqual(expected, evolved.Cells(), t)
+	var evolved = Board{starting}.Evolve()
+	AssertCellsEqual(expected, evolved.Cells(), t)
 }
 
 func AssertCellsEqual(expected Cells, actual Cells, t *testing.T) {
-  sort.Sort(expected)
-  sort.Sort(actual)
-  AssertEqual(expected, actual, t)
+	sort.Sort(expected)
+	sort.Sort(actual)
+	AssertEqual(expected, actual, t)
 }
 
 func AssertEqual(expected interface{}, actual interface{}, t *testing.T) {
-  var expectedFormat = fmt.Sprintf("%v", expected)
-  var actualFormat = fmt.Sprintf("%v", actual)
+	var expectedFormat = fmt.Sprintf("%v", expected)
+	var actualFormat = fmt.Sprintf("%v", actual)
 
-  if expectedFormat != actualFormat {
-    t.Errorf("expected %v but got %v", expectedFormat, actualFormat)
-  }
+	if expectedFormat != actualFormat {
+		t.Errorf("expected %v but got %v", expectedFormat, actualFormat)
+	}
 }
-
